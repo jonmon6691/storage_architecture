2e8095c9a4a994a6c43c5350c30408b87671c255
jonmon6691 <jonmon6691@gmail.com>
2020-11-15 11:18:11 -0800

    Update backup script Make it check the local flag so that increments are based on successful prevous uploads only. This allows local user snapshots without effecting the backup system. Also added the same verification check and flag set after increment is written.

--- backup_increment.sh
+++ backup_increment.sh
@@ -17,7 +17,7 @@ else
 	exit
 fi
 
-prev_snapshot=`zfs list -s creation -o name -pH -t snapshot tank/archives | tail -n 1`
+prev_snapshot=`zfs list -s creation -o name,tag:offsite -pH -t snapshot tank/archives | awk '$2 == "offsite" {print $1}' | tail -n 1`
 prev_stamp=`zfs list -o creation -pH -t snapshot ${prev_snapshot}`
 
 # Create snapshot
@@ -26,9 +26,14 @@ next_stamp=`zfs list -o creation -pHt snapshot ${dataset}@next`
 (set -x; zfs rename ${dataset}@next ${dataset}@${next_stamp}) || exit
 
 # Send increment
-increment_file=${prev_stamp}_${next_stamp}.zfsi
-(set -x; zfs send --raw -i ${prev_snapshot} ${dataset}@${next_stamp} > ${remote_dir}/${increment_file}) || exit
+increment_file=${remote_dir}/${prev_stamp}_${next_stamp}.zfsi
+(set -x; zfs send --raw -i ${prev_snapshot} ${dataset}@${next_stamp} > ${increment_file}) || exit
+
+# Verify increment on remote, tag snapshot if all is well
+if [[ -f ${increment_file} && `stat -c %s ${increment_file}` -gt 0 ]]
+then
+	(set -x; zfs set tag:offsite=offsite ${dataset}@${next_stamp})
+fi
 
 # Delete old snapshot
 #TODO
-
