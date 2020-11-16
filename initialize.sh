c6c97a01c230af5f5ccf7a267e61734429041703
jonmon6691 <jonmon6691@gmail.com>
2020-11-15 17:37:29 -0800

    First go at initialize rclone int., seems to be working!

--- initialize.sh
+++ initialize.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 if [[ $# -lt 2 ]]
 then
@@ -8,7 +8,7 @@ then
 fi
 
 dataset=$1
-remote_dir=$2
+remote_dir=$2/$dataset
 
 # Temp file for holding the remote directory listing cache
 files=$(tempfile)
@@ -18,32 +18,37 @@ function rmtemp {
 trap rmtemp EXIT
 
 # Check no base on remote already
-ls -1 $remote_dir > $files
-base=$(grep "base_[[:digit:]]\+.zfs$" $files)
-if [[ $base != "" ]]
+rclone lsf $remote_dir > $files
+if [[ $? -eq 0 ]]
 then
-	echo "[exists] $remote_dir/$base"
-	echo "[failed] Remote already contains a base!"
-	exit
+	base=$(grep "base_[[:digit:]]\+/$" $files)
+	if [[ $base != "" ]]
+	then
+		echo "[exists] $remote_dir/$base"
+		echo "[failed] Remote already contains a base!"
+		exit
+	fi
 fi
 
 # Create snapshot
 tmp_name=$dataset@base
 (set -x; zfs snapshot $tmp_name) || exit
 stamp=`zfs list -o creation -pHt snapshot $tmp_name`
-base_name=$dataset@$stamp_base
-(set -x; zfs rename $tmp_name $base_name) || exit
+base_name=$dataset@base_$stamp
+(set -x; sudo zfs rename $tmp_name $base_name) || exit
 
 # Verify remote
 
 # Create base_file
-base_path=$remote_dir/base_$stamp.zfs
-(set -x; zfs send --raw --replicate $base_name > $base_path) || exit
+base_path=$remote_dir/base_$stamp
+rclone mkdir $base_path
+(set -x; sudo zfs send --raw --replicate $base_name | ./rpipe/rpipe.py $base_path) || exit
 
 # Verify increment on remote, tag snapshot if all is well
-if [[ -f $base_path && `stat -c %s $base_path` -gt 0 ]]
+check=$(rclone ls $base_path/rp-aaaaaa | awk '{print $1}')
+if [[ $? -eq 0 && $check -gt 0 ]]
 then
-	(set -x; zfs set tag:offsite=offsite $base_name)
+	(set -x; sudo zfs set tag:offsite=offsite $base_name)
 fi
 
 echo "dataset=$dataset" > backup_args.bash
